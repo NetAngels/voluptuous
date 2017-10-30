c08c8e16e3adc7ed702fa0b36470b66937cbadd6
Vasiliy Sheredeko <piphon@gmail.com>
2017-10-30 12:19:57 +0500

    Fix issue with locale files

--- voluptuous/__init__.py
+++ voluptuous/__init__.py
@@ -5,5 +5,5 @@ from voluptuous.validators import *
 from voluptuous.util import *
 from voluptuous.error import *
 
-__version__ = '0.10.5'
+__version__ = '0.10.6-netangels'
 __author__ = 'tusharmakkar08'
